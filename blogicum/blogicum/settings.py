4356e17561ce7fa5ae999c5670ee31111baac205
Sergey Sokolkin <hawkxdev@gmail.com>
2025-06-17 08:41:29 +0300

    feat: update email settings and refactor static page views to class-based views

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -108,3 +108,7 @@ CSRF_FAILURE_VIEW = 'pages.views.csrf_failure'
 LOGIN_REDIRECT_URL = 'blog:index'
 
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+
+EMAIL_FILE_PATH = BASE_DIR / 'sent_emails'
--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,23 +1,31 @@
+"""Обработчики статических страниц и системных ошибок проекта."""
+
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 
-def about(request):
+class AboutPage(TemplateView):
+    """Отображает статическую страницу "О проекте"."""
     template_name = 'pages/about.html'
-    return render(request, template_name)
 
 
-def rules(request):
+class RulesPage(TemplateView):
+    """Отображает статическую страницу "Правила"."""
     template_name = 'pages/rules.html'
-    return render(request, template_name)
 
 
 def csrf_failure(request, reason=''):
+    """Обработка ошибки CSRF: подделка межсайтового запроса."""
+    _ = reason
     return render(request, 'pages/403csrf.html', status=403)
 
 
 def page_not_found(request, exception):
+    """Обработка ошибки 404: страница не найдена."""
+    _ = exception
     return render(request, 'pages/404.html', status=404)
 
 
 def server_error(request):
+    """Обработка ошибки 500: внутренняя ошибка сервера."""
     return render(request, 'pages/500.html', status=500)
