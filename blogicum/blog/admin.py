5384f3fae8d87569a5516b4c56c4b7d308184bdc
Sergey Sokolkin <hawkxdev@gmail.com>
2025-06-19 09:04:26 +0300

    feat: add Comment model to admin interface with custom display for comment text

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -1,13 +1,17 @@
-"""Настройка отображения моделей Category, Location и Post в админке."""
+"""Настройка отображения моделей Category, Location, Post
+и Comment в админке.
+"""
 
 from django.contrib import admin
+from django.template.defaultfilters import truncatechars
+from django.utils.html import format_html
 
-from .models import Category, Location, Post
+from .models import Category, Comment, Location, Post
 
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = (
-        'title', 'description', 'is_published'
+        'title', 'description', 'is_published',
     )
     list_editable = ('is_published',)
     list_filter = ('is_published',)
@@ -15,7 +19,7 @@ class CategoryAdmin(admin.ModelAdmin):
 
 class LocationAdmin(admin.ModelAdmin):
     list_display = (
-        'name', 'is_published'
+        'name', 'is_published',
     )
     list_editable = ('is_published',)
     list_filter = ('is_published',)
@@ -24,16 +28,32 @@ class LocationAdmin(admin.ModelAdmin):
 class PostAdmin(admin.ModelAdmin):
     list_display = (
         'title', 'author', 'category',
-        'location', 'pub_date', 'is_published'
+        'location', 'pub_date', 'is_published',
     )
     list_editable = ('is_published',)
     list_filter = (
         'pub_date', 'author', 'location',
-        'category', 'is_published'
+        'category', 'is_published',
     )
     empty_value_display = 'Не задано'
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        'post', 'author', 'short_text_html', 'created_at'
+    )
+    list_display_links = ('short_text_html',)
+
+    def short_text_html(self, comment) -> str:
+        """Возвращает укороченную версию текста комментария
+        с безопасным HTML-выводом для отображения в админке.
+        """
+        return format_html('{}', truncatechars(comment.text, 50))
+
+    short_text_html.short_description = 'Текст комментария'
 
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Location, LocationAdmin)
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
